bcf12645a4466e497235f6ff4e0117ec72b9f67f
David Leon <leon95024@gmail.com>
2023-10-09 13:26:56 -0500

    componente de control de gastos

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,16 @@ import Header from './components/Header'
 
 function App() {
 
-  const [presupuesto, setPresupuesto] = useState(0);
+  const [presupuesto, setPresupuesto] = useState('');
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
 
   return (
     <div>
       <Header 
         presupuesto = {presupuesto}
         setPresupuesto = {setPresupuesto}
+        isValidPresupuesto = {isValidPresupuesto}
+        setIsValidPresupuesto = {setIsValidPresupuesto}
       />
     </div>
   )
